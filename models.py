5d2e97708386ebb5d64dcac14d9d960473f02d3d
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2024-01-07 00:54:31 +0300

    customers table

--- models.py
+++ models.py
@@ -9,3 +9,10 @@ class Restaurant (Base):
     name=Column(String)
     price = Column(Integer)
     reviews = relationship('Review')
+
+class Customer(Base):
+    __tablename__='customers'
+    id= Column(Integer, primary_key=True)
+    first_name= Column(String)
+    last_name= Column(String)
+    reviews=relationship('Review')

23c7ff0c0996bc62bb58938181464be0e843f542
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2024-01-07 00:52:31 +0300

    restaurants table'

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship 
+from sqlalchemy.ext.declarative import declarative_base
+
+Base= declarative_base
+class Restaurant (Base):
+    __tablename__='restaurants'
+    id = Column(Integer, primary_key=True)
+    name=Column(String)
+    price = Column(Integer)
+    reviews = relationship('Review')

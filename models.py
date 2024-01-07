e58e17943e71ebb92cfde92f0b9f793b32c88c80
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2024-01-07 22:40:46 +0300

    review and customer table

--- models.py
+++ models.py
@@ -16,3 +16,11 @@ class Customer(Base):
     first_name= Column(String)
     last_name= Column(String)
     reviews=relationship('Review')
+class Review(Base):
+    __tablename__='reviews'
+    id= Column(Integer, primary_key=True)
+    star_rating = Column(Integer)
+    restaurant_id= Column(Integer, ForeignKey('restaurants.id'))
+    customer_id=Column(Integer, ForeignKey('customers.id'))
+    restaurant = relationship('Restaurant')
+    customer= relationship('Customer')
